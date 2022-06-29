f991a3a794d17d54e2df2a52ff8d7a0f434bfd84
nullthefirst <ninte.dev@gmail.com>
2022-06-29 21:03:49 +0100

    Included animal schema into schema types.

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,9 +5,10 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Import project schemas
+import animal from "./animal";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([animal]),
 });
